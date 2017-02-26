46a842c48e497ed26d631ea8e0dbb930836e69c3
Werner Chao <oahcrenrew@gmail.com>
2017-02-25 23:06:06 -0500

    added d zero order model

--- R/explore.R
+++ R/explore.R
@@ -2,6 +2,20 @@ library(ggplot2)
 library(dplyr)
 data <- read.csv('../data/siteA.csv')
 head(data)
-total_daily_revenue = data %>% group_by(Date_Name) %>% 
+total_daily_revenue = data %>% group_by(Date_Name, subCategory) %>% 
   summarise(daily_revenue = sum(sales)) 
-ggplot(total_daily_revenue, aes(x=Date_Name, y=daily_revenue)) + geom_point()
+
+# Boxplots for all categories
+total_daily_revenue %>%
+  ggplot(aes(x=reorder(subCategory, daily_revenue), y=daily_revenue)) + geom_boxplot()
+
+total_daily_revenue %>%
+  ggplot(aes(x=reorder(subCategory, daily_revenue, median), y=daily_revenue)) + geom_boxplot()
+
+total_daily_revenue %>% 
+  ggplot(aes(x=subCategory)) + geom_bar()
+
+total_daily_revenue %>% 
+  ggplot(aes(x=daily_revenue)) + geom_density(aes(color=subCategory))
+  # geom_histogram(binwidth=1000, aes(fill=subCategory)) +
+  # facet_wrap( ~ subCategory)
